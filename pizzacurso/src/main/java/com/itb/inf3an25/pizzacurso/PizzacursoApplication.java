6a80be8010afde30b6e0e02434e987205fd13deb
Jenyffer Borges Cavalcanti <rm90389@estudante.fieb.edu.br>
2025-02-17 18:46:03 -0300

    início Controle de versão

---
+++ pizzacurso/src/main/java/com/itb/inf3an25/pizzacurso/PizzacursoApplication.java
@@ -0,0 +1,13 @@
+package com.itb.inf3an25.pizzacurso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzacursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzacursoApplication.class, args);
+	}
+
+}
---
+++ pizzacurso/src/test/java/com/itb/inf3an25/pizzacurso/PizzacursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itb.inf3an25.pizzacurso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzacursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
